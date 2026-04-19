fb89065214d1ed7e8b8e56ee23f39bf716e4ad21
Mishal Qamar <mishalqamar@gmail.com>
2026-04-19 22:10:24 +0100

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from langgraph.graph import StateGraph, MessagesState, START
+from langgraph.graph import StateGraph, MessagesState, START, END
 from agents.classification_agent import classification_agent
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class TutorState(MessagesState):
@@ -11,5 +14,6 @@ graph_builder = StateGraph(TutorState)
 graph_builder.add_node("classification_agent", classification_agent)
 
 graph_builder.add_edge(START, "classification_agent")
+graph_builder.add_edge("classification_agent", END)
 
 graph = graph_builder.compile()
